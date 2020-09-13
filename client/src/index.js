9734f6daaf9ba5ea86c901bf598149360341c41c
unknown <jackcharp69@gmail.com>
2020-09-13 13:42:12 -0400

    MVP done

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+             <App />
+    </BrowserRouter>, 
+    document.getElementById('root'));
